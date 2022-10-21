a83fe60db1ab2569aba09a6a9896460bfd341ff7
kavikatekhaye <kavikatekhaye20@gmail.com>
2022-10-21 06:51:29 +0530

    auth done

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -92,8 +92,9 @@
                                 <b class="caret"></b>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Profile</a></li>
-                                <li><a href="#">Logout</a></li>
+                                <li><a href="">Profile</a> </li>
+                                <li><a href="">{{Auth::user()->name??''}}</a></li>
+                                <li><a href="{{route('logout')}}">Logout</a></li>
                             </ul>
                         </li>
                     </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\UserController;
-
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,24 +21,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/dashboard',[DashboardController::class,'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+Route::get('admin/dashboard',[DashboardController::class,'index'])->middleware('auth');
 // ProductController
-Route::get('admin/product/create',[ProductController::class,'create'])->name('admin.product.create');
-Route::post('admin/product/store',[ProductController::class,'store'])->name('admin.product.store');
-Route::get('admin/product/table',[ProductController::class,'table'])->name('admin.product.table');
-Route::get('admin/product/edit/{id}',[ProductController::class,'edit'])->name('admin.product.edit');
-Route::post('admin/product/update/{id}',[ProductController::class,'update'])->name('admin.product.update');
-Route::get('admin/product/delete/{id}',[ProductController::class,'delete'])->name('admin.product.delete');
-Route::get('admin/product/detail/{id}',[ProductController::class,'detail'])->name('admin.product.detail');
+Route::get('admin/product/create',[ProductController::class,'create'])->name('admin.product.create')->middleware('auth');
+Route::post('admin/product/store',[ProductController::class,'store'])->name('admin.product.store')->middleware('auth');
+Route::get('admin/product/table',[ProductController::class,'table'])->name('admin.product.table')->middleware('auth');
+Route::get('admin/product/edit/{id}',[ProductController::class,'edit'])->name('admin.product.edit')->middleware('auth');
+Route::post('admin/product/update/{id}',[ProductController::class,'update'])->name('admin.product.update')->middleware('auth');
+Route::get('admin/product/delete/{id}',[ProductController::class,'delete'])->name('admin.product.delete')->middleware('auth');
+Route::get('admin/product/detail/{id}',[ProductController::class,'detail'])->name('admin.product.detail')->middleware('auth');
 
 
 // OrderController
-Route::get('admin/order/table',[OrderController::class,'table'])->name('admin.order.table');
-Route::get('admin/order/detail/{id}',[OrderController::class,'detail'])->name('admin.order.detail');
+Route::get('admin/order/table',[OrderController::class,'table'])->name('admin.order.table')->middleware('auth');
+Route::get('admin/order/detail/{id}',[OrderController::class,'detail'])->name('admin.order.detail')->middleware('auth');
 
 // UserControllerRoute
-Route::get('admin/user/table',[UserController::class,'table'])->name('admin.user.table');
-Route::get('admin/user/detail/{id}',[UserController::class,'detail'])->name('admin.user.detail');
-
-
-
+Route::get('admin/user/table',[UserController::class,'table'])->name('admin.user.table')->middleware('auth');
+Route::get('admin/user/detail/{id}',[UserController::class,'detail'])->name('admin.user.detail')->middleware('auth');
